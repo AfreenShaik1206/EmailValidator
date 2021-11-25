cd660e02f397df7445272440564e1323edd6514a
Afreen <afreen.shaik@amzur.com>
2021-11-25 14:20:09 +0530

    Example added

---
+++ validator/EmailValidaion/EmailValidaion/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EmailValidaion
+//
+//  Created by Amzur on 25/11/21.
+//
+
+import UIKit
+import Pods_EmailValidaion
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
